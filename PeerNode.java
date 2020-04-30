53327790c0b7f13a55850bc3968ce50c6d5ab7f4
BenWRees <br7g18@soton.ac.uk>
2020-04-30 17:48:01 +0100

    Updated to include the right logger messages for votesReceived

--- PeerNode.java
+++ PeerNode.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 public class PeerNode {
 	private String peerPortNumber = "";
-	private HashSet<String> messages = new HashSet<String>();
+	private ArrayList<String> messages = new ArrayList<String>();
 	private ServerSocket serverSock = null;
 	private Map<Socket, PrintWriter> socketToOutput = new HashMap<Socket,PrintWriter>(); 
 	private ArrayList<Socket> connectionsToOtherPorts = new ArrayList<Socket>();
@@ -14,6 +14,7 @@ public class PeerNode {
 	private ArrayList<Socket> crashedPeer = new ArrayList<Socket>();
 	private ArrayList<Integer> initPorts;
 	private Integer timeout;
+	private Map<Integer,String> portToMessagePortSent = new HashMap<Integer,String>();
 
 
 	//class represents other sockets trying to reach out and send a message in 
@@ -35,10 +36,10 @@ public class PeerNode {
 	 * DOESN'T NEED TO THROW IO ERROR
 	 */
 	synchronized public void multicastSend(HashSet<String> msg) {
-		String txt = "VOTE ";
+		String txt = "VOTE";
 		//iterate through each vote 
 		for(String currentVote : msg) {
-			txt += currentVote + " ";
+			txt += " " + currentVote;
 			//message format: VOTE port vote port vote
 		}
 		//check if the 
@@ -50,7 +51,7 @@ public class PeerNode {
 		while (outputIt.hasNext()) {
 	    		PrintWriter pw = (PrintWriter) outputIt.next();
 	    		pw.println(txt);
-	    		System.out.println("MESSAGES SENT TO OTHER PEERS " + txt);
+	    		System.out.println("MESSAGES SENT: " + txt);
 	    		pw.flush();
 		}
 	}
@@ -70,18 +71,23 @@ public class PeerNode {
 	 * need to work out a way to check if for a socket you're receiving a message from if their server socket is closed
 	 * GET THIS TO THROW THE IOEXCEPTION ITSELF AND HANDLE IT ACCORDINGLY
 	 */
-	synchronized public HashSet<String> multicastReceive(Integer timeout) {
+	synchronized public ArrayList<String> multicastReceive(Integer timeout) {
 		messages.clear();
 		for(Socket socket: connectionsToOtherPorts) {
 			try {
 				if(socket.isConnected() && (!crashedPeer.contains(socket))) {
 					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 					String msg = in.readLine();
-					
 					if(msg != null) {
-						for(int i=1; i < msg.split("\\s").length; i=i+2) {
-							messages.add(msg.split("\\s")[i] + " " + msg.split("\\s")[i+1]);
+						System.out.println("MESSAGE RECEIVED: " + msg + " from " + socket.getPort());
+						//need to remove the "Vote" 
+						msg = msg.replace("VOTE ", "");
+						if(msg.equals("VOTE")) {
+							msg = "";
 						}
+						messages.add(msg);
+						portToMessagePortSent.put(socket.getPort(), msg);
+						msg = "";
 					} else {continue;}
 
 				}
@@ -98,6 +104,14 @@ public class PeerNode {
 		}
 		//messages.remove(0);
 		return messages;
+	}
+
+	public Map<Integer,String> getHashMapOfMessages() {
+		return portToMessagePortSent;
+	}
+
+	public void clearHashMapOfMessages() {
+		portToMessagePortSent.clear();
 	}
 
 	public void updateSocketsBeingReceivedFrom() {
@@ -146,7 +160,12 @@ public class PeerNode {
 
 	public void startServersTimeout(ArrayList<Integer> participants) throws SocketTimeoutException {
 		try {
-			serverSock.setSoTimeout(timeout*participants.size());
+			serverSock.setSoTimeout(timeout);
+			try {
+				TimeUnit.SECONDS.sleep(6);
+			} catch(InterruptedException e) {
+				System.out.println("Well Fuck");
+			}
 		} catch(SocketException e) {
 			System.out.println("Cant get the timeout working");
 			return;
